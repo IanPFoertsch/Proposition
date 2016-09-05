a7e49d474c46c1da36f136da782be20eb7ab1850
Ian <ian@iMac-10DDB1C5A8CE.local>
2016-09-05 19:47:58 -0400

    Moved test sentence setup into shared fixture for compound_sentence_spec

--- spec/proposition/sentence/compound_sentence_spec.rb
+++ spec/proposition/sentence/compound_sentence_spec.rb
@@ -1,19 +1,9 @@
 require 'spec_helper'
+require_relative '../sentence_fixtures'
 
 module Proposition
   RSpec.describe CompoundSentence do
-    let(:a) { AtomicSentence.new("A") }
-    let(:b) { AtomicSentence.new("B") }
-    let(:c) { AtomicSentence.new("C") }
-    let(:d) { AtomicSentence.new("D") }
-    let(:f) { AtomicSentence.new("F") }
-
-    let(:not_sentence) { CompoundSentence.new(a, Logic::NOT) }
-    let (:compound_sentence) {CompoundSentence.new(a, Logic::AND, b)}
-    let(:a_and_b) { CompoundSentence.new(a, Logic::AND, b) }
-    let(:c_implication_d) { CompoundSentence.new(c, Logic::IMPLICATION, d) }
-
-    let(:complex_compound) { CompoundSentence.new(a_and_b, Logic::XOR, c_implication_d) }
+    include_context "sentence fixtures"
 
     describe "is_unary?" do
       it "should be true for a unary Sentence" do
