32deb136898ac8c005a84155deceae2a29c79739
Ian Foertsch <IanPFo@gmail.com>
2017-01-24 07:39:18 -0500

    somewhat alphabetized autoloading classes

--- lib/proposition.rb
+++ lib/proposition.rb
@@ -4,19 +4,25 @@ require "active_support/core_ext/array"
 
 
 module Proposition
+  autoload  :And, 'proposition/sentence/binary/and'
   autoload  :AtomicSentence, 'proposition/sentence/atomic_sentence'
+  autoload  :BinarySentence, 'proposition/sentence/binary/binary_sentence'
+  autoload  :Conjunction, 'proposition/sentence/n_ary/conjunction'
+  autoload  :Clause, 'proposition/sentence/n_ary/clause'
+  autoload  :Disjunction, 'proposition/sentence/n_ary/disjunction'
+  autoload  :Evaluator, 'proposition/sentence/evaluator'
+  autoload  :KnowledgeBase, 'proposition/knowledge_base'
+  autoload  :Logic, 'proposition/sentence/logic'
+  autoload  :Not, 'proposition/sentence/not'
   autoload  :Sentence, 'proposition/sentence/sentence'
   autoload  :Processor, 'proposition/operators/processor'
 
-  autoload  :Logic, 'proposition/sentence/logic'
-  autoload  :Evaluator, 'proposition/sentence/evaluator'
-  autoload  :Not, 'proposition/sentence/not'
-  autoload  :BinarySentence, 'proposition/sentence/binary/binary_sentence'
-  autoload  :And, 'proposition/sentence/binary/and'
+
+
   autoload  :Or, 'proposition/sentence/binary/or'
-  autoload  :Conjunction, 'proposition/sentence/n_ary/conjunction'
-  autoload  :Disjunction, 'proposition/sentence/n_ary/disjunction'
-  autoload  :Clause, 'proposition/sentence/n_ary/clause'
+
+
+
   autoload  :ConjunctiveNormalForm, 'proposition/sentence/n_ary/conjunctive_normal_form'
   autoload  :NArySentence, 'proposition/sentence/n_ary/n_ary_sentence'
 
