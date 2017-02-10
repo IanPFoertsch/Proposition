d0d90fb0899e16dce625db60a14e5dddcc818a0f
Ian Foertsch <IanPFo@gmail.com>
2017-02-10 06:55:18 -0500

    set operations supported on n_ary sentences

--- spec/proposition/operators/processor_spec.rb
+++ spec/proposition/operators/processor_spec.rb
@@ -4,7 +4,19 @@ module Proposition
   RSpec.describe Processor do
     include_context "sentence fixtures"
 
-    let(:query) { c }
-    
+    describe "sets of clauses" do
+      let(:kb) { Conjunction.new([clause_a_b_c, clause_c]) }
+      let(:set) { Set.new(kb.sentences) }
+      let(:set_clause_c) { [clause_c].to_set }
+      it "should identify subsets" do
+        expect(set.subset?(set_clause_c)).to be(false)
+        expect(set_clause_c.subset?(set)).to be(true)
+      end
+    end
+
+    describe "pl_resolution" do
+      
+
+    end
   end
 end
